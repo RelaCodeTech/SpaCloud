bef197d3c118167ed5c9c627128929a105d6f002
RelacodeTech <relacode.tech@gmail.com>
2015-08-08 11:29:21 +0530

    Initial commit

---
+++ SpaCloud.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaCloud.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
